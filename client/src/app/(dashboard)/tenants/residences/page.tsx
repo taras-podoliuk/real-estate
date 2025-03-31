229aef389ef2db80e49f59b20ee4f400b7df3768
Taras Podoliuk <podolyktaras@gmail.com>
2025-03-31 19:10:36 +0300

    property dashboard pages complete

--- client/src/app/(dashboard)/tenants/residences/page.tsx
+++ client/src/app/(dashboard)/tenants/residences/page.tsx
@@ -5,51 +5,50 @@ import Header from "@/components/Header";
 import Loading from "@/components/Loading";
 import {
   useGetAuthUserQuery,
-  useGetPropertiesQuery,
+  useGetCurrentResidencesQuery,
   useGetTenantQuery,
 } from "@/state/api";
 
-const Favorites = () => {
+const Residences = () => {
   const { data: authUser } = useGetAuthUserQuery();
   const { data: tenant } = useGetTenantQuery(
     authUser?.cognitoInfo?.userId || "",
     { skip: !authUser?.cognitoInfo?.userId }
   );
   const {
-    data: favrotiteProperties,
+    data: currentResidences,
     isLoading,
     isError,
-  } = useGetPropertiesQuery(
-    { favoriteIds: tenant?.favorites?.map((fav: { id: number }) => fav.id) },
-    { skip: !tenant?.favorites || tenant?.favorites.length === 0 }
-  );
+  } = useGetCurrentResidencesQuery(authUser?.cognitoInfo?.userId || "", {
+    skip: !authUser?.cognitoInfo?.userId,
+  });
 
   if (isLoading) return <Loading />;
-  if (isError) return <div>Error loading favorites</div>;
+  if (isError) return <div>Error loading current residences</div>;
 
   return (
     <div className="dashboard-container">
       <Header
-        title="Favorited Properties"
-        subtitle="Browse and manage your saved property listings"
+        title="Current Residences"
+        subtitle="View and manage your current living spaces"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {favrotiteProperties?.map((property) => (
+        {currentResidences?.map((property) => (
           <Card
             key={property.id}
             property={property}
-            isFavorite={true}
+            isFavorite={tenant?.favorites.includes(property.id) || false}
             onFavoriteToggle={() => {}}
             showFavoriteButton={false}
             propertyLink={`/tenants/residences/${property.id}`}
           />
         ))}
       </div>
-      {(!favrotiteProperties || favrotiteProperties.length === 0) && (
-        <p>You don&apos;t have any favorited properties</p>
+      {(!currentResidences || currentResidences.length === 0) && (
+        <p>You don&apos;t have any current residences</p>
       )}
     </div>
   );
 };
 
-export default Favorites;
+export default Residences;
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,5 +1,5 @@
 import { cleanParams, createNewUserInDatabase, withToast } from "@/lib/utils";
-import { Manager, Property, Tenant } from "@/types/prismaTypes";
+import { Lease, Manager, Payment, Property, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 import { FiltersState } from ".";
@@ -17,7 +17,14 @@ export const api = createApi({
     },
   }),
   reducerPath: "api",
-  tagTypes: ["Managers", "Tenants", "Properties", "PropertyDetails"],
+  tagTypes: [
+    "Managers",
+    "Tenants",
+    "Properties",
+    "PropertyDetails",
+    "Leases",
+    "Payments",
+  ],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
       queryFn: async (_, _queryApi, _extraptions, fetchWithBQ) => {
@@ -60,7 +67,7 @@ export const api = createApi({
       },
     }),
 
-    // property related endpoints
+    // ===================== property related endpoints =====================
     getProperties: build.query<
       Property[],
       Partial<FiltersState> & { favoriteIds?: number[] }
@@ -103,7 +110,8 @@ export const api = createApi({
       providesTags: (result, error, id) => [{ type: "PropertyDetails", id }],
     }),
 
-    // tenant related endpoints
+    // ===================== tenant related endpoints =====================
+
     getTenant: build.query<Tenant, string>({
       query: (cognitoId) => `tenants/${cognitoId}`,
       providesTags: (result) => [{ type: "Tenants", id: result?.id }],
@@ -112,6 +120,17 @@ export const api = createApi({
       //     error: "Failed to load tenant profile.",
       //   });
       // },
+    }),
+
+    getCurrentResidences: build.query<Property[], string>({
+      query: (cognitoId) => `tenants/${cognitoId}/current-residences`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Properties" as const, id })),
+              { type: "Properties", id: "LIST" },
+            ]
+          : [{ type: "Properties", id: "LIST" }],
     }),
 
     updateTenantSettings: build.mutation<
@@ -165,6 +184,48 @@ export const api = createApi({
         { type: "Properties", id: "LIST" },
       ],
     }),
+
+    // ===================== manager related endpoints =====================
+
+    getManagerProperties: build.query<Property[], string>({
+      query: (cognitoId) => `managers/${cognitoId}/properties`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Properties" as const, id })),
+              { type: "Properties", id: "LIST" },
+            ]
+          : [{ type: "Properties", id: "LIST" }],
+    }),
+
+    createProperty: build.mutation<Property, FormData>({
+      query: (newProperty) => ({
+        url: `properties`,
+        method: "POST",
+        body: newProperty,
+      }),
+      invalidatesTags: (result) => [
+        { type: "Properties", id: "LIST" },
+        { type: "Managers", id: result?.manager?.id },
+      ],
+    }),
+
+    // ===================== lease related endpoints =====================
+
+    getLeases: build.query<Lease[], number>({
+      query: () => `leases`,
+      providesTags: ["Leases"],
+    }),
+
+    getPropertyLeases: build.query<Lease[], number>({
+      query: (propertyId) => `properties/${propertyId}/leases`,
+      providesTags: ["Leases"],
+    }),
+
+    getPayments: build.query<Payment[], number>({
+      query: (leaseId) => `leases/${leaseId}/payments`,
+      providesTags: ["Payments"],
+    }),
   }),
 });
 
@@ -174,7 +235,13 @@ export const {
   useUpdateTenantSettingsMutation,
   useUpdateManagerSettingsMutation,
   useGetPropertyQuery,
+  useGetCurrentResidencesQuery,
+  useGetManagerPropertiesQuery,
+  useCreatePropertyMutation,
   useGetPropertiesQuery,
   useAddFavoritePropertyMutation,
   useRemoveFavoritePropertyMutation,
+  useGetLeasesQuery,
+  useGetPropertyLeasesQuery,
+  useGetPaymentsQuery,
 } = api;
