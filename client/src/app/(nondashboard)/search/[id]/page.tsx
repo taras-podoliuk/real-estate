6a6c38932c28edc57f0be2464a047e076c5f756a
Taras Podoliuk <podolyktaras@gmail.com>
2025-04-04 14:14:23 +0300

    fixed public view of property

--- client/src/app/(nondashboard)/search/[id]/page.tsx
+++ client/src/app/(nondashboard)/search/[id]/page.tsx
@@ -22,11 +22,17 @@ const SingleListing = () => {
         images={["/singlelisting-2.jpg", "/singlelisting-3.jpg"]}
       />
       <div className="flex flex-col md:flex-row justify-center gap-10 mx-10 md:w-2/3 md:mx-auto mt-16 mb-8">
-        <div className="order-2 md:order-1">
-          <PropertyOverview propertyId={propertyId} />
-          <PropertyDetails propertyId={propertyId} />
-          <PropertyLocation propertyId={propertyId} />
-        </div>
+        {authUser ? (
+          <div className="order-2 md:order-1">
+            <PropertyOverview propertyId={propertyId} />
+            <PropertyDetails propertyId={propertyId} />
+            <PropertyLocation propertyId={propertyId} />
+          </div>
+        ) : (
+          <div className="text-xl font-semibold text-primary-700 self-center justify-self-center">
+            Sign in to view property details
+          </div>
+        )}
 
         <div className="order-1 md:order-2">
           <ContactWidget onOpenModal={() => setIsModalOpen(true)} />
